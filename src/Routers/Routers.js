19a87db5b6f15687ec0279df544add1ac0d8bb26
h-behgam <h.behgam@gmail.com>
2023-11-20 11:48:53 +0100

    Update Routers.js

--- src/Routers/Routers.js
+++ src/Routers/Routers.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import NotFound from "../Pages/NotFound";
 import Search from "../Pages/Search";
 // import Wrapper from "../Components/Wrapper/Wrapper";
@@ -7,15 +7,13 @@ import Layout from "../Pages/Layout";
 
 function Routers() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route path="/search" element={<Search />} />
-          <Route path="/search/:search" element={<Search />} />
-          <Route path="*" element={<NotFound />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route path="/search" element={<Search />} />
+        <Route path="/search/:search" element={<Search />} />
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    </Routes>
   );
 }
 
