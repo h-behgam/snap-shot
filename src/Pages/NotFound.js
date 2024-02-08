384961d9f7255bf65ba1efea9a9a683faa6631a0
h-behgam <h.behgam@gmail.com>
2024-02-08 14:56:42 +0100

    removing old file
    removing old file
    and saving all files in separate folders

--- src/Pages/NotFound.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function NotFound() {
-  return (
-    <h1 className='text-center text-capitalize'>This route is not valid. Please check a valid address</h1>
-  )
-}
-
-export default NotFound
