c3929e2cb029ee8ec59efe947aaa469f56c64f2e
h-behgam <h.behgam@gmail.com>
2024-02-10 13:38:08 +0100

    Update SnapshorSlice.ts

--- src/Redux/SnapShot/SnapshorSlice.ts
+++ src/Redux/SnapShot/SnapshorSlice.ts
@@ -1,4 +1,4 @@
-import { PayloadAction, createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
 import { stateType, title } from "./Types";
 
@@ -11,15 +11,15 @@ const initialState: stateType = {
 export const fetchData = createAsyncThunk(
   "Snapshot/fetchdata",
   async (title: title = "flower", { rejectWithValue }) => {
-    const url: string = `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=s4a7010e99aa73e312b6111242b43af75&per_page=12&format=json&nojsoncallback=1&tags=${title}`;
+    const url: string = `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=4a7010e99aa73e312b6111242b43af75&per_page=12&format=json&nojsoncallback=1&tags=${title}`;
     try {
       const res = await axios.get(url);
-      if (res.data.code !== 200) {
+      if (res.data.code && res.data.code !== 200) {
         return rejectWithValue(res.data.message);
       }
       return res.data;
-    } catch (error) {
-      //   return rejectWithValue(error);
+    } catch (error: any) {
+      return rejectWithValue(error.message);
     }
   }
 );
